3914f08313bea0b59a977f68b169ca0a4ede2f25
Harissis <163694074+Harissis@users.noreply.github.com>
2024-08-30 19:23:39 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,7 @@ Route::post('/cadastrar', [UserController::class, 'store']);
 
 Route::prefix('/user')->group(function () {
     Route::get('/', [UserController::class, 'index']);
-    
-    // Adicionando as rotas PUT e DELETE
+
     Route::put('/{id}', [UserController::class, 'update']); 
     Route::delete('/{id}', [UserController::class, 'destroy']); 
 });
