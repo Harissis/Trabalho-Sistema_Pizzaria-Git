bb37f46b01a4225b2d5cd371a9d4933131522650
Harissis <163694074+Harissis@users.noreply.github.com>
2024-08-30 19:26:02 -0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,63 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserCreateRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-/**
- * Class UserController
- *
- * @package App\Http\Controllers
- * @author Vinícius Sarmento
- * @link https://github.com/ViniciusSCS
- * @date 2024-08-23 21:48:54
- * @copyright UniEVANGÉLICA
- */
 class UserController extends Controller
 {
-    // Métodos existentes...
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        // Encontrar o usuário pelo ID
         $user = User::find($id);
 
         if (!$user) {
             return response()->json(['error' => 'Usuário não encontrado'], 404);
         }
 
-        // Atualizar os dados do usuário
         $user->update($request->all());
 
         return response()->json([
             'status' => 200,
-            'menssagem' => 'Usuário atualizado com sucesso!!',
+            'mensagem' => 'Usuário atualizado com sucesso!!',
             'user' => $user
         ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        // Encontrar o usuário pelo ID
         $user = User::find($id);
 
         if (!$user) {
             return response()->json(['error' => 'Usuário não encontrado'], 404);
         }
 
-        // Deletar o usuário
         $user->delete();
 
         return response()->json([
             'status' => 200,
-            'menssagem' => 'Usuário deletado com sucesso!!'
+            'mensagem' => 'Usuário deletado com sucesso!!'
         ]);
     }
 }
